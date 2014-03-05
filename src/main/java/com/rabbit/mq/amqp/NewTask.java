d7afafe8ac47a5d3af3a6798afe8fcc5e162159c
syedshahul <syedshahul2002@gmail.com>
2014-03-05 18:59:07 +0530

    Work Queues

---
+++ src/main/java/com/rabbit/mq/amqp/NewTask.java
@@ -0,0 +1,52 @@
+package com.rabbit.mq.amqp;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.util.Date;
+
+/**
+ * @author: Syed Shahul
+ */
+public class NewTask {
+	private final static String QUEUE_NAME = "myfirstqueue";
+	private static String getMessage(String[] strings){
+		if (strings.length < 1)
+			return "Hello World!";
+		return joinStrings(strings, " ");
+	}
+
+	private static String joinStrings(String[] strings, String delimiter) {
+		int length = strings.length;
+		if (length == 0) return "";
+		StringBuilder words = new StringBuilder(strings[0]);
+		for (int i = 1; i < length; i++) {
+			words.append(delimiter).append(strings[i]);
+		}
+		return words.toString();
+	}
+
+	public static void main(String[] argv)
+		throws java.io.IOException {
+		ConnectionFactory factory = new ConnectionFactory();
+		factory.setHost(System.getenv("HOST"));
+		factory.setVirtualHost(System.getenv("VHOST"));
+		factory.setUsername(System.getenv("UNAME"));
+		factory.setPassword(System.getenv("PASSCODE"));
+
+		Connection connection = factory.newConnection();
+		Channel channel = connection.createChannel();
+
+		channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+		//String message = "Hello World! queue"+ new Date().toString();
+		//channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
+		String message = getMessage(argv);
+
+		channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
+		System.out.println(" [x] Sent '" + message + "'");
+
+		channel.close();
+		connection.close();
+	}
+}
---
+++ src/main/java/com/rabbit/mq/amqp/Worker.java
@@ -0,0 +1,53 @@
+package com.rabbit.mq.amqp;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.QueueingConsumer;
+
+/**
+ * @author: Syed Shahul
+ */
+public class Worker {
+	private final static String QUEUE_NAME = "myfirstqueue";
+	private static void doWork(String task) throws InterruptedException {
+		for (char ch: task.toCharArray()) {
+			if (ch == '.') Thread.sleep(1000);
+		}
+	}
+
+	public static void main(String[] argv)
+		throws java.io.IOException,
+		       InterruptedException {
+
+		ConnectionFactory factory = new ConnectionFactory();
+		factory.setHost(System.getenv("HOST"));
+		factory.setVirtualHost(System.getenv("VHOST"));
+		factory.setUsername(System.getenv("UNAME"));
+		factory.setPassword(System.getenv("PASSCODE"));
+		Connection connection = factory.newConnection();
+		Channel channel = connection.createChannel();
+
+		channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+		System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+
+		QueueingConsumer consumer = new QueueingConsumer(channel);
+		boolean autoAck = false;
+		channel.basicConsume(QUEUE_NAME, autoAck, consumer);
+
+	/*	while (true) {
+			QueueingConsumer.Delivery delivery = consumer.nextDelivery();
+			String message = new String(delivery.getBody());
+			System.out.println(" [x] Received '" + message + "'");
+		}*/
+		while (true) {
+			QueueingConsumer.Delivery delivery = consumer.nextDelivery();
+			String message = new String(delivery.getBody());
+
+			System.out.println(" [x] Received '" + message + "'");
+			doWork(message);
+			channel.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
+			System.out.println(" [x] Done");
+		}
+	}
+}
